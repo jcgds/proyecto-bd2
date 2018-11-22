2b668dd555c9ffb339c3ee4c8271322e4623fc02
Angel <angelantonio3000@gmail.com>
2018-11-22 12:31:39 -0400

    TDAS y Create de tablas restantes

--- create.sql
+++ create.sql
@@ -33,6 +33,68 @@ CREATE OR REPLACE TYPE publicaciones_nt as TABLE OF varchar2(50);
 /
 CREATE OR REPLACE TYPE hechos_hist_nt as TABLE OF hechos_hist;
 /
+create or replace type conj_telefonos is varray(5) of number(14);
+/
+create or replace type direccion as object (
+    estado varchar2(100),
+    codigoPostal varchar2(15),
+    lineaDireccion1 varchar2(100),
+    lineaDireccion2 varchar2(100)
+);
+/
+create or replace type personaDeContacto as object (
+    nombre varchar2(50),
+    apellido varchar2(50),
+    cargo varchar2(50),
+    email varchar2(50)
+);
+/
+create or replace type personasDeContacto_var as vararray(2) of personaDeContacto;
+/
+create or replace type datosDeContacto as object (
+    telefonos conj_telefonos,
+    fax number(14),
+    email varchar2(50),
+    direccionWeb varchar2(100),
+    dir direccion,
+    personasDeContacto personasDeContacto_var
+);
+/
+create or replace type premio as object(
+    nombre varchar2(50),
+    posicion number(10),
+    descripcion varchar2(200),
+    tipo varchar2(10),
+    premioMoneda number(20)
+);
+/
+create or replace type premio_nt as table of premio;
+/
+create or replace type escala as object(
+    elemento varchar2(20),
+    rangoInferior number(5),
+    rangoSuperior number(5),
+    clasificacion varchar2(20)
+);
+/
+create or replace type escala_nt as table of escala;
+/
+create or replace type costoInscripcion as object(
+    nroDeMuestras number(10),
+    valor number(20),
+    unidadValor varchar2(50),
+    pais varchar2(50)
+);
+/
+create or replace type costoInscripcion_nt as table of costoInscripcion;
+/
+create or replace type datosBancarios as object(
+    recipiente varchar2(50),
+    nombreBanco varchar2(50),
+    codigoCuenta varchar2(50),
+    codigoSucursal varchar2(50)
+);
+/
 /* Creacion de tablas */
 
 create table VariedadVid (
@@ -96,5 +158,67 @@ CREATE TABLE Jueces (
     fk_catadorexperto number,
     fk_edicion number,
     CONSTRAINT pk_jueces PRIMARY KEY (id, fk_catadorexperto, fk_edicion)
+);
+/
+CREATE TABLE Concurso (
+    id number,
+    nombre varchar2(50) NOT NULL,
+    datosDeContacto datosDeContacto,
+    tipoDeCata varchar2(20) NOT NULL,
+    deCatadores varchar2(2),
+    premios premio_nt,
+    escalas escala_nt,
+    caracteristicas varchar2(200),
+    CONSTRAINT pk_concurso PRIMARY KEY (id)
+)
+NESTED TABLE premios STORE AS premio_nt_1,
+NESTED TABLE escalas STORE AS escala_nt_1;
+/
+CREATE TABLE Organizador (
+    id number,
+    nombre varchar2(50) NOT NULL,
+    descripcion varchar2(200),
+    CONSTRAINT pk_organizador PRIMARY KEY(id)
+);
+/
+CREATE TABLE Organizador_Concurso (
+    id number,
+    fk_organizador number NOT NULL,
+    fk_concurso number NOT NULL,
+    CONSTRAINT pk_organizador_concurso PRIMARY KEY(id,fk_organizador,fk_concurso)
+);
+/
+CREATE TABLE P_O (
+    id number,
+    fk_organizador number NOT NULL,
+    fk_pais number NOT NULL,
+    CONSTRAINT pk_p_o PRIMARY KEY (id,fk_organizador,fk_pais)
+);
+/
+CREATE TABLE Edicion(
+    id number,
+    datosBancarios datosBancarios,
+    fechaLimiteEnvioDeInscripcion date NOT NULL,
+    fechaLimiteRecepcionVinos date,
+    fechaInicio date NOT NULL,
+    fechaFin date NOT NULL,
+    precioEstandarPorMuestra number(10),
+    direccionE direccion NOT NULL,
+    costos costoInscripcion_nt,
+    lugarRealizar lugar NOT NULL,
+    unidadMonetaria varchar2(20) NOT NULL,
+    emailEnvioInscripcion varchar2(50),
+    CONSTRAINT pk_edicion PRIMARY KEY (id)
+)
+NESTED TABLE costos STORE AS costoInscripcion_nt_1;
+/
+CREATE TABLE Inscripcion (
+    id number,
+    fecha date NOT NULL,
+    premioCatador varchar2(50),
+    fk_edicion number NOT NULL,
+    fk_bodega number,
+    fk_catadoraprendiz number,
+    CONSTRAINT pk_inscripcion PRIMARY KEY (id)
 );
 /
