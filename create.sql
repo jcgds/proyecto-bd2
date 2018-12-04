76cfe0da59ef128e1eddcd910d7a2aeb7e0dd6ba
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-04 10:57:29 -0400

    Cambiados ';' por '/' para poder correr en SQLPLus

--- create.sql
+++ create.sql
@@ -1,57 +1,55 @@
---ALTER SESSION SET CURRENT_SCHEMA = WINE_SCHEMA;
--- /
-/* Definicion de TDAs */
-
+ALTER SESSION SET CURRENT_SCHEMA = WINE_SCHEMA
+/
 create or replace type tipo_valor as object (
     anio number(4, 0),
     valor number,
     unidad varchar2(30)
-);
+)
 /
 create or replace type distribucion_exp as object (
     tipoValor tipo_valor,
     pais varchar2(50)
-);
+)
 /
 CREATE OR REPLACE TYPE valoracion as object (
     nombreElemento varchar2(50),
     valor number,
     observacion varchar2(100)
-);
+)
 /
 CREATE OR REPLACE TYPE lugar as object (
     ciudad varchar2(50),
     pais varchar2(50)
-);
+)
 /
 CREATE OR REPLACE TYPE hechos_hist as object (
     anio number(10),
     hechos varchar2(100)
-);
+)
 /
-CREATE OR REPLACE TYPE valoracion_nt as TABLE OF valoracion;
+CREATE OR REPLACE TYPE valoracion_nt as TABLE OF valoracion
 /
-CREATE OR REPLACE TYPE publicaciones_nt as TABLE OF varchar2(50);
+CREATE OR REPLACE TYPE publicaciones_nt as TABLE OF varchar2(50)
 /
-CREATE OR REPLACE TYPE hechos_hist_nt as TABLE OF hechos_hist;
+CREATE OR REPLACE TYPE hechos_hist_nt as TABLE OF hechos_hist
 /
-create or replace type conj_telefonos is varray(5) of number(14);
+create or replace type conj_telefonos is varray(5) of number(14)
 /
 create or replace type direccion as object (
     estado varchar2(100),
     codigoPostal varchar2(15),
     lineaDireccion1 varchar2(100),
     lineaDireccion2 varchar2(100)
-);
+)
 /
 create or replace type personaDeContacto as object (
     nombre varchar2(50),
     apellido varchar2(50),
     cargo varchar2(50),
     email varchar2(50)
-);
+)
 /
-create or replace type personasDeContacto as varray(3) of personaDeContacto;
+create or replace type personasDeContacto as varray(3) of personaDeContacto
 /
 create or replace type datosDeContacto as object (
     telefonos conj_telefonos,
@@ -60,11 +58,11 @@ create or replace type datosDeContacto as object (
     direccionWeb varchar2(100),
     dir direccion,
     contactos personasDeContacto
-);
+)
 /
-create or replace type tipo_valor_nt as table of tipo_valor;
+create or replace type tipo_valor_nt as table of tipo_valor
 /
-create or replace type distribucion_exp_nt as table of distribucion_exp;
+create or replace type distribucion_exp_nt as table of distribucion_exp
 /
 create or replace type premio as object(
     nombre varchar2(50),
@@ -72,62 +70,63 @@ create or replace type premio as object(
     descripcion varchar2(200),
     tipo varchar2(10),
     premioMoneda number(20)
-);
+)
 /
-create or replace type premio_nt as table of premio;
+create or replace type premio_nt as table of premio
 /
 create or replace type escala as object(
     elemento varchar2(20),
     rangoInferior number(5),
     rangoSuperior number(5),
     clasificacion varchar2(20)
-);
+)
 /
-create or replace type escala_nt as table of escala;
+create or replace type escala_nt as table of escala
 /
 create or replace type costoInscripcion as object(
     nroDeMuestras number(10),
     valor number(20),
     unidadValor varchar2(50),
     pais varchar2(50)
-);
+)
 /
-create or replace type costoInscripcion_nt as table of costoInscripcion;
+create or replace type costoInscripcion_nt as table of costoInscripcion
 /
 create or replace type datosBancarios as object(
     recipiente varchar2(50),
     nombreBanco varchar2(50),
     codigoCuenta varchar2(50),
     codigoSucursal varchar2(50)
-);
+)
 /
 create or replace type unidadMonetaria as object(
     nombre varchar2(50),
     simbolo varchar2(5)
-);
+)
 /
 create or replace type calificacion as object(
     nombreCritica varchar2(50),
     valor tipo_valor
-);
+)
 /
-create or replace type calificacion_nt as table of calificacion;
+create or replace type calificacion_nt as table of calificacion
 /
-create or replace type maridajes as varray(5) of varchar2(50);
+create or replace type maridajes as varray(5) of varchar2(50)
 /
 create or replace type rangoPorcentajeVol as object(
     desde number,
     hasta number
-);
+)
 /
-CREATE OR REPLACE DIRECTORY mapas_regionales AS 'C:\WINE_DB\mapas_regionales';
 
-/* Creacion de tablas */
+CREATE OR REPLACE DIRECTORY mapas_regionales AS 'C:\WINE_DB\mapas_regionales'
+/
+
 -- Tablas Juan
 CREATE TABLE Pais (
     id number,
     nombre varchar2(50) not null,
-    continente varchar2(9) not null CHECK (continente IN ('Asia', 'Europa', 'África', 'América', 'Oceanía', 'Antártida')),
+    continente varchar2(9) not null CHECK (continente IN ('Asia', 'Europa', 'ï¿½frica', 'Amï¿½rica', 'Oceanï¿½a', 'Antï¿½rtida')),
     superficieVinedo tipo_valor_nt,
     produccionAnual tipo_valor_nt,
     exportacionAnual distribucion_exp_nt,
@@ -138,23 +137,26 @@ CREATE TABLE Pais (
 )
 NESTED TABLE superficieVinedo STORE AS superficieVinedo_nt_pais
 NESTED TABLE produccionAnual STORE AS produccionAnual_nt_pais
-NESTED TABLE exportacionAnual STORE AS exportacionAnual_nt_pais;
+NESTED TABLE exportacionAnual STORE AS exportacionAnual_nt_pais
 /
+
 create table Region (
     id number,
     nombre varchar2(50) not null,
     descripcion varchar2(100),
     FK_Pais number not null,
     constraint pk_region primary key (id)
-);
+)
 /
+
 create table VariedadVid (
     id number(10),
     nombre varchar2(50) not null,
     --tipo varchar2(6) check (tipo in ('tinta','blanca')) not null,
     constraint pk_variedadvid primary key (id)
-);
+)
 /
+
 create table DenominacionDeOrigen (
     id number(10),
     nombre varchar2(50) not null,
@@ -162,8 +164,9 @@ create table DenominacionDeOrigen (
     FK_VariedadVid number(10),
     FK_Region number,
     constraint pk_denominacion_de_origen primary key (id, FK_VariedadVid, FK_Region)
-);
+)
 /
+
 create table Bodega (
     id number(10),
     nombre varchar2(50) not null,
@@ -179,8 +182,7 @@ create table Bodega (
 )
 NESTED TABLE historia STORE AS historia_nt_bodega
 NESTED TABLE produccionAnual STORE AS produccionAnual_nt_bodega
-NESTED TABLE exportacionAnual STORE AS exportacionAnual_nt_bodega;
---NESTED TABLE datosDeContacto.personasDeContacto STORE AS personasDeContacto_nt_bodega;
+NESTED TABLE exportacionAnual STORE AS exportacionAnual_nt_bodega
 /
 
 create table B_DO (
@@ -192,8 +194,9 @@ create table B_DO (
     --------------------------------
     fk_bodega number(10),
     constraint pk_B_DO primary key (id, fk_do_id, fk_do_VariedadVid, fk_do_region, fk_bodega)
-);
+)
 /
+
 create table Cosecha (
     id number,
     anio number(4, 0) not null,
@@ -211,10 +214,11 @@ create table Cosecha (
     --------------------------------------
     constraint clasificacion_valida check (clasificacion in ('E', 'MB', 'R', 'D')),
     constraint pk_cosecha primary key (id, fk_bdo_id, fk_bdo_do_id, fk_bdo_do_VariedadVid, fk_bdo_do_region, fk_bdo_bodega)
-);
+)
+/
 
 -- Tablas Cagua
-/
+
 CREATE TABLE CatadorAprendiz(
     pasaporte number,
     nombre varchar2(50) NOT NULL,
@@ -224,8 +228,9 @@ CREATE TABLE CatadorAprendiz(
     lugarNacimiento lugar,
     CONSTRAINT tipo_genero CHECK (genero in ('M','F','O')),
     CONSTRAINT pk_catadoraprendiz PRIMARY KEY (pasaporte)
-);
+)
 /
+
 CREATE TABLE CataAprendiz (
     id number,
     fecha date NOT NULL,
@@ -244,8 +249,9 @@ CREATE TABLE CataAprendiz (
     -------------------------------------------------
     CONSTRAINT pk_cataaprendiz PRIMARY KEY (id)
 )
-NESTED TABLE valoraciones STORE AS valoracion_nt_1;
+NESTED TABLE valoraciones STORE AS valoracion_nt_1
 /
+
 CREATE TABLE CatadorExperto (
     id number,
     nombre varchar2(50) NOT NULL,
@@ -261,8 +267,9 @@ CREATE TABLE CatadorExperto (
     CONSTRAINT pk_catadorexperto PRIMARY KEY (id)
 )
 NESTED TABLE hechosCurriculum STORE AS hechosCurriculum_nt_1
-NESTED TABLE publicaciones STORE AS publicaciones_nt_1;
+NESTED TABLE publicaciones STORE AS publicaciones_nt_1
 /
+
 CREATE TABLE CataExperto (
     id number,
     fecha date NOT NULL,
@@ -272,15 +279,17 @@ CREATE TABLE CataExperto (
     fk_muestracompite number,
     CONSTRAINT pk_cataexperto PRIMARY KEY (id)
 )
-NESTED TABLE valoraciones STORE AS valoracion_nt_2;
+NESTED TABLE valoraciones STORE AS valoracion_nt_2
 /
+
 CREATE TABLE Juez (
     id number,
     fk_catadorexperto number,
     fk_edicion number,
     CONSTRAINT pk_juez PRIMARY KEY (id, fk_catadorexperto, fk_edicion)
-);
+)
 /
+
 CREATE TABLE Concurso (
     id number,
     nombre varchar2(50) NOT NULL,
@@ -293,29 +302,33 @@ CREATE TABLE Concurso (
     CONSTRAINT pk_concurso PRIMARY KEY (id)
 )
 NESTED TABLE premios STORE AS premio_nt_1
-NESTED TABLE escalas STORE AS escala_nt_1;
+NESTED TABLE escalas STORE AS escala_nt_1
 /
+
 CREATE TABLE Organizador (
     id number,
     nombre varchar2(50) NOT NULL,
     descripcion varchar2(200),
     CONSTRAINT pk_organizador PRIMARY KEY(id)
-);
+)
 /
+
 CREATE TABLE Organizador_Concurso (
     id number,
     fk_organizador number NOT NULL,
     fk_concurso number NOT NULL,
     CONSTRAINT pk_organizador_concurso PRIMARY KEY(id,fk_organizador,fk_concurso)
-);
+)
 /
+
 CREATE TABLE P_O (
     id number,
     fk_organizador number NOT NULL,
     fk_pais number NOT NULL,
     CONSTRAINT pk_p_o PRIMARY KEY (id,fk_organizador,fk_pais)
-);
+)
 /
+
 CREATE TABLE Edicion(
     id number,
     datosBancarios datosBancarios,
@@ -333,8 +346,9 @@ CREATE TABLE Edicion(
     fk_concurso number NOT NULL,
     CONSTRAINT pk_edicion PRIMARY KEY (id)
 )
-NESTED TABLE costos STORE AS costoInscripcion_nt_1;
+NESTED TABLE costos STORE AS costoInscripcion_nt_1
 /
+
 CREATE TABLE Inscripcion (
     id number,
     fecha date NOT NULL,
@@ -343,8 +357,9 @@ CREATE TABLE Inscripcion (
     fk_bodega number,
     fk_catadoraprendiz number,
     CONSTRAINT pk_inscripcion PRIMARY KEY (id)
-);
+)
 /
+
 CREATE TABLE HistoricoPrecio(
     anio date,
     -- PK Presentacion -----------
@@ -354,8 +369,9 @@ CREATE TABLE HistoricoPrecio(
     ------------------------------
     precio number NOT NULL,
     CONSTRAINT pk_historicoprecio PRIMARY KEY(anio, fk_presentacion, fk_marcavino,fk_clasificacionvinos)
-);
+)
 /
+
 CREATE TABLE Presentacion(
     id number,
     -- PK MarcaVino --------------
@@ -366,8 +382,9 @@ CREATE TABLE Presentacion(
     unidadesEnCaja number,
     CONSTRAINT pk_presentacion PRIMARY KEY(id, fk_marcavino, fk_clasificacionvinos),
     CHECK (tipo IN ('Botella', 'Caja'))
-);
+)
 /
+
 CREATE TABLE MuestraCompite(
     id number,
     -- PK MarcaVino -------------
@@ -379,8 +396,9 @@ CREATE TABLE MuestraCompite(
     premio premio_nt, -- TODO: Creo que es un unico premio no varios (Verificar con profesora)
     CONSTRAINT pk_muestracompite PRIMARY KEY(id, fk_inscripcion)
 )
-NESTED TABLE premio STORE AS premio_nt_muestracompite;
+NESTED TABLE premio STORE AS premio_nt_muestracompite
 /
+
 CREATE TABLE MarcaVino_B_DO(
     id number,
     -- PK MarcaVino ---------------
@@ -395,8 +413,9 @@ CREATE TABLE MarcaVino_B_DO(
     fk_do_region number,
     -------------------------------
     CONSTRAINT pk_marcavino_b_do PRIMARY KEY(id, fk_marcavino, fk_clasificacionvinos, fk_b_do, fk_denominaciondeorigen, fk_do_VariedadVid, fk_do_region, fk_bodega)
-);
+)
 /
+
 CREATE TABLE ClasificacionVinos(
     id number,
     fk_clasificacionvinos number,
@@ -404,8 +423,9 @@ CREATE TABLE ClasificacionVinos(
     nivel number NOT NULL,
     porcentajes rangoPorcentajeVol NOT NULL,
     CONSTRAINT pk_clasificacionvinos PRIMARY KEY(id)
-);
+)
 /
+
 CREATE TABLE MarcaVino(
     id number,
     fk_clasificacionvinos number,
@@ -430,8 +450,9 @@ CREATE TABLE MarcaVino(
 )
 NESTED TABLE produccionAnual STORE AS produccionAnual_MarcaVino
 NESTED TABLE exportacionAnual STORE AS exportacionAnual_MarcaVino
-NESTED TABLE criticas STORE AS criticas_MarcaVino;
+NESTED TABLE criticas STORE AS criticas_MarcaVino
 /
+
 CREATE TABLE MuestraCatador(
   id number,
   -- PK Marca Vino ------------------
@@ -446,4 +467,5 @@ CREATE TABLE MuestraCatador(
   anada number NOT NULL,
   sumatoriaExperto number NOT null,
   CONSTRAINT pk_muestracatador PRIMARY KEY (id, fk_marcavino_id, fk_marcavino_clasificacion, fk_juez, fk_edicion, fk_catadorexperto)
-);
+)
+/
