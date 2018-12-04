50c3113a7bcba28e995a8dc057710c17d0508d5a
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-04 11:01:06 -0400

    Creada funcion que calcula la produccion total de una bodega en un año

--- procedimientos.sql
+++ procedimientos.sql
@@ -1,7 +1,29 @@
---ALTER SESSION SET CURRENT_SCHEMA=WINE_SCHEMA;
-
+ALTER SESSION SET CURRENT_SCHEMA=WINE_SCHEMA;
+/
 create or replace procedure saludar(nombre varchar2) as
 begin
     DBMS_OUTPUT.PUT_LINE('Hola ' || nombre || '!');
 end;
-
+/
+create or replace function produccion_bodega_en (idBodega IN number, anioDeseado IN number)
+return number is 
+    produccion number := 0;
+BEGIN
+   
+    FOR rec IN (select t.anio anio, t.valor valor
+        from the (
+            select mv.produccionAnual 
+            from Marcavino mv, MarcaVino_B_DO mvbdo
+            where mvbdo.fk_bodega = idBodega
+                and mvbdo.fk_marcavino = mv.id  
+                and mvbdo.fk_clasificacionvinos = mv.fk_clasificacionvinos
+        ) t
+        where t.anio = anioDeseado)
+    LOOP
+        -- DBMS_OUTPUT.PUT_LINE ('Anio: ' || TO_CHAR(rec.anio) || ' Prod: ' || TO_CHAR(rec.valor));
+        produccion := produccion + rec.valor;
+    END LOOP;
+    
+    return produccion;
+END;
+/
