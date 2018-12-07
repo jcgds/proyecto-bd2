2e3ea34581d01c93cf993d5ec4d5c00af3ba9187
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-07 11:31:42 -0400

    Adaptar para que puedda correr en SQLplus

--- alter.sql
+++ alter.sql
@@ -19,68 +19,68 @@ alter table Cosecha add
 
 ALTER TABLE CataAprendiz ADD CONSTRAINT fk_CA FOREIGN KEY (fk_catadoraprendiz) 
     REFERENCES CatadorAprendiz (pasaporte);
-/
+
 ALTER TABLE CataAprendiz ADD CONSTRAINT fk_M FOREIGN KEY (fk_muestra_id, fk_muestra_marcavino_id, 
                                                           fk_muestra_marcavino_clasif, fk_juez_id, fk_edicion, fk_catadorexperto)
     REFERENCES MuestraCatador (id, fk_marcavino_id, fk_marcavino_clasificacion, fk_juez, fk_edicion, fk_catadorexperto);
-/
+
 ALTER TABLE CataExperto ADD CONSTRAINT fk_CE FOREIGN KEY (fk_catadorexperto)
     REFERENCES CatadorExperto (id);
-/
+
 ALTER TABLE Juez ADD CONSTRAINT fk_Edicion_Juez FOREIGN KEY (fk_edicion)
     REFERENCES Edicion (id);
-/
+
 ALTER TABLE Juez ADD CONSTRAINT fk_CatadorExperto FOREIGN KEY (fk_catadorexperto)
     REFERENCES CatadorExperto (id);
-/
+
 ALTER TABLE Organizador_Concurso ADD CONSTRAINT fk_O FOREIGN KEY (fk_organizador)
     REFERENCES Organizador (id);
-/
+
 ALTER TABLE Organizador_Concurso ADD CONSTRAINT fk_Concurso FOREIGN KEY (fk_concurso)
     REFERENCES Concurso (id);
-/
+
 ALTER TABLE P_O ADD CONSTRAINT fk_Organizador FOREIGN KEY (fk_organizador)
     REFERENCES Organizador (id);
-/
+
 ALTER TABLE P_O ADD CONSTRAINT fk_Organizador_Pais FOREIGN KEY (fk_pais)
     REFERENCES Pais (id);
-/    
+    
 ALTER TABLE Edicion ADD CONSTRAINT fk_Concurso_Edicion FOREIGN KEY (fk_concurso)
     REFERENCES Concurso (id);
-/
+
 ALTER TABLE Inscripcion ADD CONSTRAINT fk_EI FOREIGN KEY (fk_edicion)
     REFERENCES Edicion(id);
-/
+
 ALTER TABLE Inscripcion ADD CONSTRAINT fk_CI FOREIGN KEY (fk_catadoraprendiz)
     REFERENCES CatadorAprendiz (pasaporte);
-/
+
 ALTER TABLE HistoricoPrecio ADD CONSTRAINT fk_histpre FOREIGN KEY (fk_presentacion, fk_marcavino,fk_clasificacionvinos)
     REFERENCES Presentacion (id, fk_marcavino,fk_clasificacionvinos);
-/
+
 ALTER TABLE Presentacion ADD CONSTRAINT fk_presentacion FOREIGN KEY (fk_marcavino, fk_clasificacionvinos)
     REFERENCES MarcaVino (id, fk_clasificacionvinos);
-/
+
 ALTER TABLE MarcaVino ADD CONSTRAINT fk_marcavino FOREIGN KEY (fk_clasificacionvinos)
     REFERENCES ClasificacionVinos (id);
-/
+
 ALTER TABLE ClasificacionVinos ADD CONSTRAINT fk_clasificacionvinos FOREIGN KEY (fk_clasificacionvinos)
     REFERENCES ClasificacionVinos (id);
-/
+
 ALTER TABLE MuestraCompite ADD CONSTRAINT fk_muestra_marca FOREIGN KEY (fk_marcavino, fk_clasificacionvinos)
     REFERENCES MarcaVino (id, fk_clasificacionvinos);
-/
+
 ALTER TABLE MuestraCompite ADD CONSTRAINT fk_muestra_inscripcion FOREIGN KEY (fk_inscripcion)
     REFERENCES Inscripcion (id);
-/
+
 ALTER TABLE MuestraCatador ADD CONSTRAINT fk_muestraCom_marca FOREIGN KEY (fk_marcavino_id, fk_marcavino_clasificacion)
     REFERENCES MarcaVino (id, fk_clasificacionvinos);
-/
+
 ALTER TABLE MuestraCatador ADD CONSTRAINT fk_muestraCom_juez FOREIGN KEY (fk_juez, fk_edicion, fk_catadorexperto)
     REFERENCES Juez (id, fk_edicion, fk_catadorexperto);
-/
+
 ALTER TABLE MarcaVino_B_DO ADD CONSTRAINT fk_MarcaBDO_marcavino FOREIGN KEY (fk_marcavino, fk_clasificacionvinos)
     REFERENCES MarcaVino (id, fk_clasificacionvinos);
-/
+
 ALTER TABLE MarcaVino_B_DO ADD CONSTRAINT fk_MarcaBDO_bdo FOREIGN KEY (fk_b_do, fk_bodega, fk_denominaciondeorigen,fk_do_VariedadVid,fk_do_region)
     REFERENCES B_DO (id, fk_bodega, fk_do_id,fk_do_VariedadVid,fk_do_region);
 
