61058cb23e31187c4e21540a211d1f006e65850b
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-06 20:12:59 -0400

    Insercion de segunda marca de vino de bodega Quinta da Fronteira. Insercion de segunda bodega de Portugal.

--- insert.sql
+++ insert.sql
@@ -46,7 +46,8 @@ insert into Pais values (1, 'Portugal', 'Europa', tipo_valor_nt(), tipo_valor_nt
 insert into Region values (1, 'Douro', null, 1);
 insert into VariedadVid values (1,'Touriga Nacional');
 insert into VariedadVid values (2,'Touriga Franca');
-insert into VariedadVid values (3,'Tinta Roriz');
+insert into VariedadVid values (3,'Viosinho');
+insert into VariedadVid values (4,'CÃ³dega do Lorinho');
 
 insert into DenominacionDeOrigen (id, nombre, descripcion, FK_VariedadVid, FK_Region) values 
 (1,'DOC Douro', null, 1, 1);
@@ -61,8 +62,8 @@ insert into Bodega values (
         null, 
         'info@companhiadasquintas.pt', 
         'http://companhiadasquintas.com/en/our-estates/quinta-da-fronteira',
-        direccion('Freixo de Espada à Cinta', '5180-909', 'Apartado 17 - 5180-909 Freixo de Espada à Cinta', null),
-        personasDeContacto(personaDeContacto('José','Menezes','Winemaker','douro@companhiadasquintas.pt'))
+        direccion('Freixo de Espada Ã© Cinta', '5180-909', 'Apartado 17 - 5180-909 Freixo de Espada Ã© Cinta', null),
+        personasDeContacto(personaDeContacto('JosÃ©','Menezes','Winemaker','douro@companhiadasquintas.pt'))
     ),
     'Created with the purpose of becoming one of the most important players in the Portuguese wine business.' || 
     ' Its strategy is based on the development of high quality estate wines from 250 hectares of own vineyards,' || 
@@ -70,13 +71,13 @@ insert into Bodega values (
     'TODO: Tal vez deberia poder ser null (Descripcion general vinos)',
     tipo_valor_nt(),  -- TODO: Conseguir y llenar esta tabla con 3 anios
     distribucion_exp_nt(), -- TODO: Conseguir y llenar esta tabla con 3 anios
-    null
+    null -- TODO: Crear bodega Companhia Das Quintas y colocar su id
 );
 
 insert into MarcaVino values (
     1,
     17,
-    'Quinta Da Fronteira Reserva Tinto',
+    'Reserva Tinto',
     'Grapes were handpicked to 16 kg cases and carefully selected at the winery. The fermentation and maceration were made in conical vats and were adapted to each grape variety as well as the malolatic. Tinta Roriz has done the malolactic fermentation in oak barrels; Touriga Franca has done on skins (sulphited and racked directly to barrel);and Touriga Nacional in vats. The wines were racked to the barrels before spring time. They aged for 18 months in French oak barrels (20% new).',
     'Deep ruby colour, with complex aromas of black ripe fruit, spices, mocha, orange blossom and rockrose hints. In the mouth it reveals a good balance between acidity and soft layered tannins. A long-lasting aromatic taste.',
     17,
@@ -104,9 +105,70 @@ insert into B_DO (id, fk_do_id, fk_do_variedadvid, fk_do_region, fk_bodega) valu
 insert into MarcaVino_B_DO (id, fk_marcavino, fk_clasificacionvinos, fk_b_do, fk_bodega, fk_denominaciondeorigen, fk_do_variedadvid, fk_do_region) values
 (1, 1, 17, 1, 1, 1, 1, 1); -- Quinta Da Fronteira - DOC Douro - Reserva Tinto
 
------------------------------------------------------------------------------
-insert into Pais values (4, 'Estados Unidos', 'América', tipo_valor_nt(), tipo_valor_nt(tipo_valor(2016,26900000,'hl')), distribucion_exp_nt(), 
-    unidadMonetaria('Dolar', 'US'), bfilename('mapas_regionales', 'usa.jpg'), null);
+insert into MarcaVino values (
+    3,
+    9,
+    'Fronteira White',
+    'Grapes were handpicked to small 16 kg boxes. A soft pressing was followed by a cold settling of the must, which, after reaching the required purity, was fermented for 4 weeks in stainless steel vats at a controlled temperature of 14 -16Â°C. The wine aged 5 months on fine lees in stainless steel vats which improved its complexity.',
+    'Bright citrus coloured wine with lush aroma displaying hints of white fruit (peach) and tropical (pineapple), with some mineral notes typical of the Douro region. On the palate it shows great balance, lively acidity and a lingering after taste.',
+    11,
+    'N',
+    3,
+    12.5,
+    tipo_valor_nt(tipo_valor(2011, 585000, 'litros'), tipo_valor(2012, 635000, 'litros'), tipo_valor(2013, 675000, 'litros')),
+    distribucion_exp_nt(), -- TODO: Conseguir o inventar datos de exportacion
+    calificacion_nt(
+        calificacion('Wine Enthusiast Magazine', tipo_valor(2015, 88, 'puntos')),
+    ),
+    5,
+    10,
+    maridajes('Pescados', 'Ensaladas', 'Pastas', 'Carnes blancas'),
+    6.75,
+    'Corcho 1+1',
+    null -- TODO: Ver como se sube un BLOB
+);
+
+insert into MarcaVino_B_DO (id, fk_marcavino, fk_clasificacionvinos, fk_b_do, fk_bodega, fk_denominaciondeorigen, fk_do_variedadvid, fk_do_region) values
+-- TODO: Preguntar a la profe qlq con lo de VariedadVid porque este vino usa dos variedades en la misma DOC
+--       De hecho como esta ahorita me esta guardando la variedad de vid que no corresponde a este vino.
+(1, 3, 9, 1, 1, 1, 1, 1); -- Quinta Da Fronteira - DOC Douro - Fronteira White
+
+
+------------------------------------------------------------------------------------------------------------------------------------------------
+
+-- Bodega Herade Da Farizoa --------------------------------------------------------------------------------------------------------------------
+insert into Region values (3, 'Alentejo', null, 1);
+
+insert into DenominacionDeOrigen (id, nombre, descripcion, FK_VariedadVid, FK_Region) values 
+(1,'Regional Alentejo', 'Borba', 1, 3);
+
+insert into Bodega values (
+    3, 
+    'Herdade Da Farizoa',
+    hechos_hist_nt(hechos_hist(1958, 'FundaciÃ³n de la bodega')),
+    TO_DATE('22-06-1958', 'DD-MM-YYYY'), 
+    DATOSDECONTACTO(
+        conj_telefonos(351268657552), 
+        null, 
+        'info@companhiadasquintas.pt', 
+        'http://companhiadasquintas.com/en/our-estates/herdade-da-farizoa',
+        direccion('Alentejo', '7350-491', 'Herdade da Farizoa - 7350- 491 Terrugem - Elvas', null),
+        personasDeContacto(personaDeContacto('Joaquim','Mendes','Winemaker','farizoa@companhiadasquintas.pt'))
+    ),
+    -- TODO: En este caso como son sub bodegas de Companhia Das Quintas la mision deberia ser la misma?
+    'Created with the purpose of becoming one of the most important players in the Portuguese wine business.' || 
+    ' Its strategy is based on the development of high quality estate wines from 250 hectares of own vineyards,' || 
+    ' planted in the major Portuguese wine regions. The company aims to achieve sustained growth, with main focus on foreign markets.',
+    'TODO: Tal vez deberia poder ser null (Descripcion general vinos)',
+    tipo_valor_nt(),
+    distribucion_exp_nt(),
+    null -- TODO: Crear bodega Companhia Das Quintas y colocar su id
+);
+
+----------------------------------------------------------------------------------------------------------------------------------------------
+
+insert into Pais values (4, 'Estados Unidos', 'Amï¿½rica', tipo_valor_nt(), tipo_valor_nt(tipo_valor(2016,26900000,'hl')), distribucion_exp_nt(), 
+    unidadMonetaria('Dolar', 'USD'), bfilename('mapas_regionales', 'usa.jpg'), null);
 -- Bodega OPUS ONE -----------------------------------------------    
 insert into Region values (2, 'Oakville AVA', null, 4);
 
@@ -129,8 +191,8 @@ insert into Bodega values (
         direccion('Oakville, CA', '94562', '7900 St. Helena Highway', null),
         personasDeContacto(personaDeContacto('John','Cena','Winemaker','johnc@opuswinery.com'))
     ),
-    'En Opus One la esencia del tiempo se expresa por el carácter de cada cosecha.' || 
-    'El lugar, a menudo definido como "terroir", representa la geografía, el clima y el elemento humano esencial que se captura en el equilibrio 
+    'En Opus One la esencia del tiempo se expresa por el carï¿½cter de cada cosecha.' || 
+    'El lugar, a menudo definido como "terroir", representa la geografï¿½a, el clima y el elemento humano esencial que se captura en el equilibrio 
      del vino entre la fuerza y la finura, la estructura y la textura.',
     'TODO: Tal vez deberia poder ser null (Descripcion general vinos)',
     tipo_valor_nt(),  -- TODO: Conseguir y llenar esta tabla con 3 anios
@@ -141,7 +203,7 @@ insert into MarcaVino values (
     2,
     16,
     'Overture',
-    'Overture es una expresión atemporal de los viñedos de nuestra finca, con cuerpo pero accesible en su juventud. Overture muestra una abundancia de aromas y sabores de frutas silvestres con un paladar medio suave y sutil.',
+    'Overture es una expresiï¿½n atemporal de los viï¿½edos de nuestra finca, con cuerpo pero accesible en su juventud. Overture muestra una abundancia de aromas y sabores de frutas silvestres con un paladar medio suave y sutil.',
     'Deep dark fruits and cherries. Leather, black pepper. Light but long lasting taste of grapes at the end. Paired with garlic roasted lamb rack',
     14,
     'N',
