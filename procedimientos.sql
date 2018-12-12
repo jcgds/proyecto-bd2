9ebbec1fc58609e45940fa8b6d1bb1b88d5233a3
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-12 11:28:31 -0400

    Separados procedimientos para calcular produccion y exportacion total de bodegas. Reemplazada implementacion de la funcion produccion_bodega_en. Finalizado y probado el procedimiento CalcularProduccionTotal.

--- procedimientos.sql
+++ procedimientos.sql
@@ -18,24 +18,20 @@ create or replace function produccion_bodega_en (idBodega IN number, anioDeseado
 return number is 
     produccion number := 0;
 BEGIN
-   -- TODO: Rehcaer este metodo para que solo use su NT de produccionAnual
-    FOR rec IN (select t.anio anio, t.valor valor
+       select t.valor into produccion
         from the (
-            select mv.produccionAnual 
-            from Marcavino mv, MarcaVino_B_DO mvbdo
-            where mvbdo.fk_bodega = idBodega
-                and mvbdo.fk_marcavino = mv.id  
-                and mvbdo.fk_clasificacionvinos = mv.fk_clasificacionvinos
+            select produccionAnual 
+            from Bodega
+            where id = idBodega
         ) t
-        where t.anio = anioDeseado)
-    LOOP
-        -- DBMS_OUTPUT.PUT_LINE ('Anio: ' || TO_CHAR(rec.anio) || ' Prod: ' || TO_CHAR(rec.valor));
-        produccion := produccion + rec.valor;
-    END LOOP;
-    
+        where t.anio = anioDeseado;
     return produccion;
+EXCEPTION
+    WHEN NO_DATA_FOUND THEN
+        return 0;
 END;
 /
+
 create or replace function produccion_pais_en (idPais IN number, anioDeseado IN number)
 return number is 
     produccion number := 0;
@@ -116,34 +112,28 @@ END;
 /
 /*
     Este procedimiento se encarga de calcular y actualizar los valores de produccion
-    y exportacion de todas las bodegas segun la produccion de sus marcas de vino.
+    de todas las bodegas segun la produccion de sus marcas de vino.
     
-    Al ser ejecutado elimina todos los registros de las NT produccionAnual y exportacionAnual 
-    de las bodegas y los reemplaza calculando de nuevo la produccion y exportacion segun las 
-    marcas de vino entre el año minimo y maximo.
+    Al ser ejecutado elimina todos los registros de las NT produccionAnual de las bodegas 
+    y los reemplaza calculando de nuevo la produccion segun las marcas de 
+    vino entre el año minimo y maximo.
 */
-create or replace procedure calcular_produccion_bodegas as
+create or replace procedure CalcularProduccionTotalBodegas as
 anio_min number := conseguir_anio_minimo();
 anio_max number;
 acumulador1 number := 0;
-type pais_exportacion is table of number index by varchar2(50);
-distribucionExp pais_exportacion;
-paisIndex varchar2(50);
 begin    
     select to_char(sysdate, 'YYYY') into anio_max from dual;
     << bodegas_loop >>
     for bodega in (select id, nombre from Bodega)
     loop
         DBMS_OUTPUT.PUT_LINE('Bodega ' || bodega.nombre || ' | ' || to_char(bodega.id));
-
         DELETE FROM THE (select vv.produccionAnual from Bodega vv where vv.id = bodega.id);
-        DELETE FROM THE (select vv.exportacionAnual from Bodega vv where vv.id = bodega.id);
 
         -- Recorremos los años y vamos sumando la produccion de las marcas de vino de la bodega en ese año
         << year_loop >>
         for i in anio_min .. anio_max loop
             acumulador1 := 0;
-            distribucionExp.Delete();
             -- Este query consigue todas las marcas de vino de la Bodega actual en el loop
             << marcas_loop >>
             for marca in (
@@ -155,8 +145,57 @@ begin
             loop
                 DBMS_OUTPUT.PUT_LINE('Marca ' || marca.nombre);
                 -- Vamos sumando la produccion de las marcas en el acumulador de la bodega
-                acumulador1 := acumulador1 + produccion_marca_en(marca.id, i);
-                
+                acumulador1 := acumulador1 + produccion_marca_en(marca.id, i);                         
+            end loop marcas_loop;
+            
+            DBMS_OUTPUT.PUT_LINE('Produccion anio ' || TO_CHAR(i) || ' es: ' || to_char(acumulador1));
+              
+            -- Guardar en produccionAnual de la Bodega este valor
+            INSERT INTO THE (select vv.produccionAnual from Bodega vv where vv.id = bodega.id)
+            values (tipo_valor(i, acumulador1, 'litros'));
+            
+        end loop year_loop;  
+    end loop bodegas_loop;  
+end;
+/
+
+/*
+    Este procedimiento se encarga de calcular y actualizar los valores de exportacion
+    de todas las bodegas segun la produccion de sus marcas de vino.
+    
+    Al ser ejecutado elimina todos los registros de las NT exportacionAnual de las bodegas 
+    y los reemplaza calculando de nuevo la exportacion segun las marcas de 
+    vino y pais entre el año minimo y maximo.
+*/
+create or replace procedure CalcExportacionTotalBodegas as
+anio_min number := conseguir_anio_minimo();
+anio_max number;
+type pais_exportacion is table of number index by varchar2(50);
+distribucionExp pais_exportacion;
+paisIndex varchar2(50);
+begin    
+    select to_char(sysdate, 'YYYY') into anio_max from dual;
+    << bodegas_loop >>
+    for bodega in (select id, nombre from Bodega)
+    loop
+        DBMS_OUTPUT.PUT_LINE('Bodega ' || bodega.nombre || ' | ' || to_char(bodega.id));
+        DELETE FROM THE (select vv.exportacionAnual from Bodega vv where vv.id = bodega.id);
+
+        -- Recorremos los años y vamos sumando la produccion de las marcas de vino de la bodega en ese año
+        << year_loop >>
+        for i in anio_min .. anio_max loop
+            distribucionExp.Delete();
+            -- Este query consigue todas las marcas de vino de la Bodega actual en el loop
+            << marcas_loop >>
+            for marca in (
+                    select distinct mv.id, mv.nombre
+                    from MarcaVino_B_DO mvbdo, MarcaVino mv
+                    where mvbdo.fk_bodega = bodega.id
+                        and mvbdo.fk_marcavino = mv.id
+                        and mvbdo.fk_clasificacionvinos = mv.fk_clasificacionvinos)
+            loop
+                DBMS_OUTPUT.PUT_LINE('Marca ' || marca.nombre);
+
                 -- Este query consigue el valor y pais de las exportaciones de la marca de vino actual
                 -- en el año del loop
                 << distribucion_loop >>
@@ -176,13 +215,7 @@ begin
                 end loop distribucion_loop;
                               
             end loop marcas_loop;
-            
-            DBMS_OUTPUT.PUT_LINE('Produccion year ' || TO_CHAR(i) || ' es: ' || to_char(acumulador1));
-              
-            -- Guardar en produccionAnual de la Bodega este valor
-            INSERT INTO THE (select vv.produccionAnual from Bodega vv where vv.id = bodega.id)
-            values (tipo_valor(i, acumulador1, 'litros'));
-            
+                                             
             -- Recorremos los valores en el hashmap y vamos guardando en la exportacionAnual de la bodega
             paisIndex := distribucionExp.first;
             << assoc_array_loop >>
@@ -193,10 +226,56 @@ begin
                 values (distribucion_exp(tipo_valor(i, distribucionExp(paisIndex), 'litros'), paisIndex));
                 
                 paisIndex := distribucionExp.next(paisIndex);
-            end loop assoc_array_loop;
+            end loop assoc_array_loop;          
             
         end loop year_loop;  
     end loop bodegas_loop;  
 end;
 /
 
+/*
+    Calcula y actualiza los valores de produccion anual de todos los paises,
+    actualizando primero los valores de produccion de las bodegas mediante el
+    metodo CalcularProduccionTotalBodegas().
+*/
+create or replace procedure CalcularProduccionTotal as
+anio_min number := conseguir_anio_minimo();
+anio_max number;
+acumulador number := 0;
+begin
+    select to_char(sysdate, 'YYYY') into anio_max from dual;
+
+    CalcularProduccionTotalBodegas();
+    -- Se termino el procedimiento anterior asi que la produccionAnual de las Bodegas deberian estar actualizadas.
+    
+    << pais_loop >>
+    for PAIS in (select p.id, p.nombre from Pais p)
+    loop     
+        DELETE FROM THE (select vv.produccionAnual from Pais vv where vv.id = PAIS.id);
+
+        << year_loop >>
+        for ANIO in anio_min .. anio_max loop
+            acumulador := 0;
+            
+            << bodega_loop >>
+            for BODEGA in (
+                select distinct bo.id, bo.nombre  
+                from Bodega bo, Region rg, DenominacionDeOrigen do, B_DO bdo
+                where rg.fk_pais = PAIS.id
+                and bdo.fk_do_region = rg.id
+                and bo.id = bdo.fk_bodega)
+            loop
+                acumulador := acumulador + produccion_bodega_en(BODEGA.id, ANIO);
+            end loop bodega_loop;     
+            
+            DBMS_OUTPUT.PUT_LINE( PAIS.nombre || ' en ' || TO_CHAR(anio) || ' produjo: ' || to_char(acumulador));
+
+            INSERT INTO THE (select vv.produccionAnual from Pais vv where vv.id = PAIS.id)
+            values (tipo_valor(anio, acumulador, 'litros'));
+            
+        end loop year_loop;
+                
+    end loop pais_loop;
+    
+end;
+/
