fcf863a7fe982e242506c87c5a4de25453d2fb5a
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-14 17:27:43 -0400

    Pedir datos de direccion y llamar al procedimiento almacenado insertar_concurso con los datos recibidos.

--- Procedures/crear_concurso.sql
+++ Procedures/crear_concurso.sql
@@ -1,12 +1,12 @@
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
- SET FEEDBACK OFF;
+SET FEEDBACK OFF;
     BEGIN
         DBMS_OUTPUT.PUT_LINE(' --- Datos para creacion de concurso ---');
     END;
     /
-    SET FEEDBACK ON;
+SET FEEDBACK ON;
     
 ACCEPT c_nombre PROMPT 'Ingrese el nombre del concurso: ';
 ACCEPT c_tipoDeCata PROMPT 'Ingrese el tipo de cata: ';
@@ -20,7 +20,16 @@ ACCEPT c_caracteristicas PROMPT 'Ingrese las caracteristicas del concurso: ';
     ACCEPT c_dc_dirWeb PROMPT 'Introduzca la direccion web del concurso: ';
 
     -- Datos para direccion
-    -- TODO: Pedirlos
+    SET FEEDBACK OFF;
+    BEGIN
+        DBMS_OUTPUT.PUT_LINE(' --- Datos para direccion ---');
+    END;
+    /
+    SET FEEDBACK ON;
+    ACCEPT c_estado PROMPT 'Ingrese el estado: ';
+    ACCEPT c_cod_postal PROMPT 'Ingrese el codigo postal: ';
+    ACCEPT c_linea1 PROMPT 'Linea de direccion 1: ';
+    ACCEPT c_linea2 PROMPT 'Linea de direccion 2: ';
 
     -- Datos para persona de contacto
     SET FEEDBACK OFF;
@@ -30,28 +39,41 @@ ACCEPT c_caracteristicas PROMPT 'Ingrese las caracteristicas del concurso: ';
     /
     SET FEEDBACK ON;
     
-    ACCEPT c_dc_persona_nombre PROMPT 'Ingrese el nombre de la persona de contacto: ';
-    ACCEPT c_dc_persona_apellido PROMPT 'Ingrese el apellido de la persona de contacto: ';
-    ACCEPT c_dc_persona_cargo PROMPT 'Ingrese el cargo de la persona de contacto: ';
-    ACCEPT c_dc_persona_email PROMPT 'Ingrese el email de la persona de contacto: ';
+    ACCEPT c_persona_nombre PROMPT 'Ingrese el nombre de la persona de contacto: ';
+    ACCEPT c_persona_apellido PROMPT 'Ingrese el apellido de la persona de contacto: ';
+    ACCEPT c_persona_cargo PROMPT 'Ingrese el cargo de la persona de contacto: ';
+    ACCEPT c_persona_email PROMPT 'Ingrese el email de la persona de contacto: ';
 
+BEGIN
+    WINE_SCHEMA.insertar_concurso(
+        '&c_nombre',
+        datosDeContacto(
+            conj_telefonos(&c_dc_telefono1),
+            &c_dc_fax,
+            '&c_dc_email',
+            '&c_dc_dirWeb',
+            direccion(
+                '&c_estado',
+                '&c_cod_postal',
+                '&c_linea1',
+                '&c_linea2'
+            ),
+            personasDeContacto(
+                personaDeContacto(
+                    '&c_persona_nombre',
+                    '&c_persona_apellido',
+                    '&c_persona_cargo',
+                    '&c_persona_email'
+                )
+            )
+        ),
+        '&c_tipoDeCata',
+        '&c_deCatadores',
+        '&c_caracteristicas'
+    );
 
-create or replace procedure crear_concurso as
-    c_nombre varchar2(50) := '&c_nombre';
-    c_tipoDeCata varchar2(50) := '&c_tipoDeCata';
-    c_deCatadores varchar2(50) := '&c_deCatadores';
-    c_caracteristicas varchar2(50) := '&c_caracteristicas';
-    c_dc_telefono1 NUMBER := &c_dc_telefono1;
-    c_dc_fax NUMBER := &c_dc_fax;
-    c_dc_email varchar2(50) := '&c_dc_email';
-    c_dc_dirWeb varchar2(50) := '&c_dc_dirWeb';
-    c_dc_persona_nombre varchar2(50) := '&c_dc_persona_nombre';
-    c_dc_persona_apellido varchar2(50) := '&c_dc_persona_apellido';
-    c_dc_persona_cargo varchar2(50) := '&c_dc_persona_cargo';
-    c_dc_persona_email varchar2(50) := '&c_dc_persona_email';
-begin
-    DBMS_OUTPUT.PUT_LINE('Leido: ' || TO_CHAR(c_dc_telefono1));
-end;
+    DBMS_OUTPUT.PUT_LINE('Para agregar un premio al concurso ejecute: @insertar_premio');
+    DBMS_OUTPUT.PUT_LINE('Para agregar una escala al concurso ejecute: @insertar_escala');
+END;
 /
 SET VERIFY ON;
-execute crear_concurso();
