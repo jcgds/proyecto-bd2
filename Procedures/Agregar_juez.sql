3dff1bfd86038b3e6d8ee753e9dabc2e2dd5a49e
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-16 17:53:55 -0400

    Ajustado orden en que se pide el input.

--- Procedures/Agregar_juez.sql
+++ Procedures/Agregar_juez.sql
@@ -1,8 +1,8 @@
 SET SERVEROUTPUT ON;
 SET VERIFY OFF;
 
-ACCEPT idCatador PROMPT 'Ingrese el catador que actua como juez: ';
-ACCEPT idEdicion PROMPT 'Ingrese la edicion de la que es juez: ';
+ACCEPT idCatador PROMPT 'Ingrese el id del catador experto que actua como juez: ';
+ACCEPT idEdicion PROMPT 'Ingrese el id de la edicion de la que es juez: ';
 
 begin
 
