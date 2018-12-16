b21f45c0b558ae585f69f94f725b959fe328c60b
alchu33 <alchu33@gmail.com>
2018-12-15 20:02:58 -0400

    Insertar_muestra_catador

--- Inicializacion/procedimientos.sql
+++ Inicializacion/procedimientos.sql
@@ -4,8 +4,8 @@ create or replace procedure insertar_concurso(pid number DEFAULT ids_seq.nextval
 begin
     INSERT INTO Concurso VALUES (
       pid,
-      nombre, 
-      dc, 
+      nombre,
+      dc,
       tipoCata,
       deCatadores,
       premio_nt(),
@@ -18,12 +18,12 @@ begin
 end;
 /
 create or replace function produccion_bodega_en (idBodega IN number, anioDeseado IN number)
-return number is 
+return number is
     produccion number := 0;
 BEGIN
        select t.valor into produccion
         from the (
-            select produccionAnual 
+            select produccionAnual
             from Bodega
             where id = idBodega
         ) t
@@ -36,13 +36,13 @@ END;
 /
 
 create or replace function produccion_pais_en (idPais IN number, anioDeseado IN number)
-return number is 
+return number is
     produccion number := 0;
 BEGIN
-   
+
     select t.valor into produccion
         from the (
-            select produccionAnual 
+            select produccionAnual
             from Pais
             where pais.id = idPais
         ) t
@@ -54,18 +54,18 @@ EXCEPTION
 END;
 /
 create or replace function produccion_marca_en (idMarca IN number, anioDeseado IN number)
-return number is 
+return number is
     produccion number := 0;
 BEGIN
-   
+
     select t.valor into produccion
         from the (
-            select produccionAnual 
+            select produccionAnual
             from MarcaVino
             where id = idMarca
         ) t
         where t.anio = anioDeseado;
-        
+
         return produccion;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
@@ -78,7 +78,7 @@ END;
     de produccion anual de las marcas de vino.
 */
 create or replace function conseguir_anio_minimo
-return number is 
+return number is
     anio number;
     loopYear number;
     minYear number := NULL;
@@ -88,25 +88,25 @@ begin
     loop
         select min(anio) into loopYear
         from the (select produccionAnual from MarcaVino where id = marca.id);
-        
+
         if (minYear is null or loopYear < minYear) then
             minYear := loopYear;
-        end if; 
+        end if;
     end loop marca_loop;
-    
+
     return minYear;
 end;
 /
 
 create or replace function exportacion_marca_en (idMarca IN number, anioDeseado IN number)
-return number is 
+return number is
     exportacion number := 0;
 BEGIN
-   
+
     select SUM(t.tipovalor.valor) into exportacion
     from the (select z.exportacionAnual from MarcaVino z where z.id = idMarca) t
     where t.tipovalor.anio = anioDeseado;
-        
+
     return exportacion;
 EXCEPTION
     WHEN NO_DATA_FOUND THEN
@@ -116,16 +116,16 @@ END;
 /*
     Este procedimiento se encarga de calcular y actualizar los valores de produccion
     de todas las bodegas segun la produccion de sus marcas de vino.
-    
-    Al ser ejecutado elimina todos los registros de las NT produccionAnual de las bodegas 
-    y los reemplaza calculando de nuevo la produccion segun las marcas de 
+
+    Al ser ejecutado elimina todos los registros de las NT produccionAnual de las bodegas
+    y los reemplaza calculando de nuevo la produccion segun las marcas de
     vino entre el a�o minimo y maximo.
 */
 create or replace procedure CalcularProduccionTotalBodegas as
 anio_min number := conseguir_anio_minimo();
 anio_max number;
 acumulador1 number := 0;
-begin    
+begin
     select to_char(sysdate, 'YYYY') into anio_max from dual;
     << bodegas_loop >>
     for bodega in (select id, nombre from Bodega)
@@ -148,26 +148,26 @@ begin
             loop
                 DBMS_OUTPUT.PUT_LINE('Marca ' || marca.nombre);
                 -- Vamos sumando la produccion de las marcas en el acumulador de la bodega
-                acumulador1 := acumulador1 + produccion_marca_en(marca.id, i);                         
+                acumulador1 := acumulador1 + produccion_marca_en(marca.id, i);
             end loop marcas_loop;
-            
+
             DBMS_OUTPUT.PUT_LINE('Produccion anio ' || TO_CHAR(i) || ' es: ' || to_char(acumulador1));
-              
+
             -- Guardar en produccionAnual de la Bodega este valor
             INSERT INTO THE (select vv.produccionAnual from Bodega vv where vv.id = bodega.id)
             values (tipo_valor(i, acumulador1, 'litros'));
-            
-        end loop year_loop;  
-    end loop bodegas_loop;  
+
+        end loop year_loop;
+    end loop bodegas_loop;
 end;
 /
 
 /*
     Este procedimiento se encarga de calcular y actualizar los valores de exportacion
     de todas las bodegas segun la produccion de sus marcas de vino.
-    
-    Al ser ejecutado elimina todos los registros de las NT exportacionAnual de las bodegas 
-    y los reemplaza calculando de nuevo la exportacion segun las marcas de 
+
+    Al ser ejecutado elimina todos los registros de las NT exportacionAnual de las bodegas
+    y los reemplaza calculando de nuevo la exportacion segun las marcas de
     vino y pais entre el a�o minimo y maximo.
 */
 create or replace procedure CalcExportacionTotalBodegas as
@@ -176,7 +176,7 @@ anio_max number;
 type pais_exportacion is table of number index by varchar2(50);
 distribucionExp pais_exportacion;
 paisIndex varchar2(50);
-begin    
+begin
     select to_char(sysdate, 'YYYY') into anio_max from dual;
     << bodegas_loop >>
     for bodega in (select id, nombre from Bodega)
@@ -202,9 +202,9 @@ begin
                 -- Este query consigue el valor y pais de las exportaciones de la marca de vino actual
                 -- en el a�o del loop
                 << distribucion_loop >>
-                for dist in (select nt.tipovalor.valor valor, nt.pais pais from the 
-                            (select mv.exportacionAnual from MarcaVino mv 
-                            where mv.id = marca.id) nt 
+                for dist in (select nt.tipovalor.valor valor, nt.pais pais from the
+                            (select mv.exportacionAnual from MarcaVino mv
+                            where mv.id = marca.id) nt
                             where nt.tipovalor.anio = i)
                 loop
                     --DBMS_OUTPUT.PUT_LINE('Pais: ' || dist.pais || ' Valor: ' || to_char(dist.valor) || ' Anio: ' || to_char(i));
@@ -216,23 +216,23 @@ begin
                             distribucionExp(dist.pais) := dist.valor;
                     end;
                 end loop distribucion_loop;
-                              
+
             end loop marcas_loop;
-                                             
+
             -- Recorremos los valores en el hashmap y vamos guardando en la exportacionAnual de la bodega
             paisIndex := distribucionExp.first;
             << assoc_array_loop >>
             while (paisIndex is not null) loop
                 DBMS_OUTPUT.PUT_LINE('Anio: ' || to_char(i) || ' Pais: ' || paisIndex || ' Valor: ' || to_char(distribucionExp(paisIndex)));
-                
+
                 INSERT INTO THE (select vv.exportacionAnual from Bodega vv where vv.id = bodega.id)
                 values (distribucion_exp(tipo_valor(i, distribucionExp(paisIndex), 'litros'), paisIndex));
-                
+
                 paisIndex := distribucionExp.next(paisIndex);
-            end loop assoc_array_loop;          
-            
-        end loop year_loop;  
-    end loop bodegas_loop;  
+            end loop assoc_array_loop;
+
+        end loop year_loop;
+    end loop bodegas_loop;
 end;
 /
 
@@ -250,36 +250,36 @@ begin
 
     CalcularProduccionTotalBodegas();
     -- Se termino el procedimiento anterior asi que la produccionAnual de las Bodegas deberian estar actualizadas.
-    
+
     << pais_loop >>
     for PAIS in (select p.id, p.nombre from Pais p)
-    loop     
+    loop
         DELETE FROM THE (select vv.produccionAnual from Pais vv where vv.id = PAIS.id);
 
         << year_loop >>
         for ANIO in anio_min .. anio_max loop
             acumulador := 0;
-            
+
             << bodega_loop >>
             for BODEGA in (
-                select distinct bo.id, bo.nombre  
+                select distinct bo.id, bo.nombre
                 from Bodega bo, Region rg, DenominacionDeOrigen do, B_DO bdo
                 where rg.fk_pais = PAIS.id
                 and bdo.fk_do_region = rg.id
                 and bo.id = bdo.fk_bodega)
             loop
                 acumulador := acumulador + produccion_bodega_en(BODEGA.id, ANIO);
-            end loop bodega_loop;     
-            
+            end loop bodega_loop;
+
             DBMS_OUTPUT.PUT_LINE( PAIS.nombre || ' en ' || TO_CHAR(anio) || ' produjo: ' || to_char(acumulador));
 
             INSERT INTO THE (select vv.produccionAnual from Pais vv where vv.id = PAIS.id)
             values (tipo_valor(anio, acumulador, 'litros'));
-            
+
         end loop year_loop;
-                
+
     end loop pais_loop;
-    
+
 end;
 /
 
@@ -292,29 +292,29 @@ paisIndex varchar2(50);
 begin
     select to_char(sysdate, 'YYYY') into anio_max from dual;
     CalcExportacionTotalBodegas();
-    
+
     << pais_loop >>
     for PAIS in (select p.id, p.nombre from Pais p)
-    loop     
+    loop
         DELETE FROM THE (select vv.exportacionAnual from Pais vv where vv.id = PAIS.id);
-        
+
          << year_loop >>
         for ANIO in anio_min .. anio_max loop
             distribucionExp.Delete();
-            
+
             << bodega_loop >>
             for BODEGA in (
-                select distinct bo.id, bo.nombre  
+                select distinct bo.id, bo.nombre
                 from Bodega bo, Region rg, DenominacionDeOrigen do, B_DO bdo
                 where rg.fk_pais = PAIS.id
                 and bdo.fk_do_region = rg.id
                 and bo.id = bdo.fk_bodega)
             loop
-            
+
                 << distribucion_loop >>
-                for dist in (select nt.tipovalor.valor valor, nt.pais pais from the 
-                            (select bo.exportacionAnual from Bodega bo 
-                            where bo.id = BODEGA.id) nt 
+                for dist in (select nt.tipovalor.valor valor, nt.pais pais from the
+                            (select bo.exportacionAnual from Bodega bo
+                            where bo.id = BODEGA.id) nt
                             where nt.tipovalor.anio = ANIO)
                 loop
                     begin
@@ -324,22 +324,22 @@ begin
                             distribucionExp(dist.pais) := dist.valor;
                     end;
                 end loop distribucion_loop;
-                
-            end loop bodega_loop;     
+
+            end loop bodega_loop;
 
             paisIndex := distribucionExp.first;
             << assoc_array_loop >>
             while (paisIndex is not null) loop
                 DBMS_OUTPUT.PUT_LINE('Anio: ' || to_char(ANIO) || ' Pais: ' || paisIndex || ' Valor: ' || to_char(distribucionExp(paisIndex)));
-                
+
                 INSERT INTO THE (select vv.exportacionAnual from Pais vv where vv.id = PAIS.id)
                 values (distribucion_exp(tipo_valor(ANIO, distribucionExp(paisIndex), 'litros'), paisIndex));
-                
+
                 paisIndex := distribucionExp.next(paisIndex);
             end loop assoc_array_loop;
-            
+
         end loop year_loop;
-        
+
     end loop pais_loop;
 end;
 /
@@ -355,14 +355,14 @@ begin
     select t.valor into produccionEnElAnio
     from the (select produccionAnual from Pais where id = p_idPais) t
     where t.anio = p_anio;
-    
+
     select SUM(t.tipovalor.valor) into exportacionEnElAnio
     from the (select exportacionAnual from Pais where id = p_idPais) t
     where t.tipovalor.anio = p_anio;
-    
+
     DBMS_OUTPUT.PUT_LINE('-- ' || nombrePais || ' -------------------------------');
     DBMS_OUTPUT.PUT_LINE('Produccion en ' || to_char(p_anio) || ': ' || to_char(produccionEnElAnio));
-    DBMS_OUTPUT.PUT_LINE('Exportacion en ' || to_char(p_anio) || ': ' || to_char(exportacionEnElAnio));   
+    DBMS_OUTPUT.PUT_LINE('Exportacion en ' || to_char(p_anio) || ': ' || to_char(exportacionEnElAnio));
     DBMS_OUTPUT.PUT_LINE('Consumo interno en ' || to_char(p_anio) || ': ' || to_char(produccionEnElAnio - exportacionEnElAnio));
     DBMS_OUTPUT.PUT_LINE('------------------------------------------------');
 end;
@@ -375,7 +375,7 @@ begin
     select contactoConMadera into holder
     from MarcaVino
     where id = idMarca;
-    
+
     if holder like 'S' then
         return 'S�';
     else
@@ -393,27 +393,27 @@ begin
     select maridaje into mar
     from MarcaVino
     where id = idMarca;
-    
+
     total := mar.count;
     for i in 1 .. total loop
         if holder is null then
             holder := mar(i) || ', ';
         elsif (i != total) then
             holder := holder || mar(i) || ', ';
-        else 
+        else
             holder := holder || mar(i);
         end if;
     end loop;
-    
+
     return holder;
 end;
 /
 
-create or replace procedure insertar_premio_a(idConcurso number, nombre varchar2, p_posicion number, descripcion varchar2, tipo varchar2, premioMoneda number) 
+create or replace procedure insertar_premio_a(idConcurso number, nombre varchar2, p_posicion number, descripcion varchar2, tipo varchar2, premioMoneda number)
 as
     premiosDePosExistentes number := 0;
 begin
-    
+
     if premioMoneda < 0 then
         RAISE_APPLICATION_ERROR(-20100, 'El valor monetario del premio no puede ser negativo');
     end if;
@@ -422,7 +422,7 @@ begin
         RAISE_APPLICATION_ERROR(-20101, 'La posicion del premio no puede ser menor a 1');
     end if;
 
-    select count(*) into premiosDePosExistentes 
+    select count(*) into premiosDePosExistentes
     from the (select premios from concurso where id = idConcurso) nt
     where nt.posicion = p_posicion;
 
@@ -439,13 +439,13 @@ begin
 end;
 /
 
-create or replace procedure insertar_escala_a(idConcurso number, p_elemento varchar2, p_rangoinf number, p_rangosup number, p_clasif varchar2) 
+create or replace procedure insertar_escala_a(idConcurso number, p_elemento varchar2, p_rangoinf number, p_rangosup number, p_clasif varchar2)
 as
     premiosDePosExistentes number := 0;
 begin
 
     -- TODO: Tal vez se deberia bloquear la insersion de valores negativos en los rangos
-    
+
     INSERT INTO THE (SELECT escalas from Concurso where id = idConcurso) VALUES
     (escala(p_elemento, p_rangoinf, p_rangosup, p_clasif));
 
@@ -455,7 +455,7 @@ begin
 end;
 /
 
-create or replace procedure agregar_organizador(idConcurso number, p_nombre varchar2, p_descripcion varchar2 DEFAULT null, fk_pais number) 
+create or replace procedure agregar_organizador(idConcurso number, p_nombre varchar2, p_descripcion varchar2 DEFAULT null, fk_pais number)
 as
     orgId number := ids_seq.nextval;
     poId number := ids_seq.nextval;
@@ -476,27 +476,27 @@ end;
 /
 
 create or replace function validar_concurso_internacional (idConcurso IN number)
-return varchar2 is 
+return varchar2 is
     esInternacional varchar2(50);
-    cont number := 0; 
+    cont number := 0;
 BEGIN
-    for Pais in (select P.nombre from 
-                        P_O PO, Organizador O,Concurso C, Organizador_Concurso OC,Pais P where 
-                        PO.fk_organizador = O.id and Po.fk_pais = P.id and OC.fk_organizador = O.id 
+    for Pais in (select P.nombre from
+                        P_O PO, Organizador O,Concurso C, Organizador_Concurso OC,Pais P where
+                        PO.fk_organizador = O.id and Po.fk_pais = P.id and OC.fk_organizador = O.id
                         and OC.fk_concurso = idConcurso) loop
 
-        if cont = 0 then   
+        if cont = 0 then
             esInternacional := Pais.nombre;
         elsif esInternacional <> Pais.nombre then
             esInternacional := 'S';
         end if;
-        cont := cont + 1;       
+        cont := cont + 1;
     end loop;
     return esInternacional;
 END;
 /
 
-create or replace procedure insertar_costo_a(idEdicion number, p_nroMuestras number, p_valor number, p_unidadValor varchar2, p_pais varchar2) 
+create or replace procedure insertar_costo_a(idEdicion number, p_nroMuestras number, p_valor number, p_unidadValor varchar2, p_pais varchar2)
 as
 begin
 
@@ -509,7 +509,7 @@ begin
 end;
 /
 
-create or replace procedure insertar_premio_a_vino(idMuestraC number,idFkInscripcion number,p_nombre varchar2, p_posicion number, p_descripcion varchar2, p_tipo varchar2,p_premioMoneda number) 
+create or replace procedure insertar_premio_a_vino(idMuestraC number,idFkInscripcion number,p_nombre varchar2, p_posicion number, p_descripcion varchar2, p_tipo varchar2,p_premioMoneda number)
 as
     idConcurso number;
     existePremio char(1);
@@ -523,9 +523,9 @@ begin
         RAISE_APPLICATION_ERROR(-20201, 'La posicion del premio no puede ser menor a 1');
     end if;
 
-    select C.id into idConcurso from Concurso C, MuestraCompite M, Inscripcion I, Edicion E 
-        where E.id = I.fk_edicion and E.fk_concurso = C.id and M.id = idMuestraC and M.fk_inscripcion = idFkInscripcion 
-        and idFkInscripcion = I.id; 
+    select C.id into idConcurso from Concurso C, MuestraCompite M, Inscripcion I, Edicion E
+        where E.id = I.fk_edicion and E.fk_concurso = C.id and M.id = idMuestraC and M.fk_inscripcion = idFkInscripcion
+        and idFkInscripcion = I.id;
 
     for nombrePremio in (select t.nombre from the(select premios from Concurso where id = idConcurso) t ) loop
         if nombrePremio.nombre = p_nombre then
@@ -533,10 +533,10 @@ begin
         end if;
     end loop;
 
-    if existePremio is null then 
+    if existePremio is null then
         RAISE_APPLICATION_ERROR(-20203, 'El premio no existe en el concurso al que se esta inscribiendo');
     end if;
-    
+
     INSERT INTO THE (SELECT premio from MuestraCompite where id = idMuestraC and fk_inscripcion = idFkInscripcion) VALUES
     (premio(p_nombre,p_posicion, p_descripcion, p_tipo,p_premioMoneda));
 
@@ -545,3 +545,37 @@ begin
     DBMS_OUTPUT.PUT_LINE('------------------------------------------------');
 end;
 /
+create or replace procedure insertar_muestra_catador(pid number DEFAULT ids_seq.nextval, anada number, sumatoriaExperto number, idMarca number, idJuez number)
+as
+idEdicion number:= 0;
+idCatadorExperto number:= 0;
+idClasificacionVino number:= 0;
+begin
+    Select fk_edicion into idEdicion
+    From Juez
+    Where id = idJuez;
+
+    Select fk_catadorexperto into idCatadorExperto
+    From Juez
+    Where id = idJuez;
+
+    Select fk_clasificacionvinos into idClasificacionVino
+    From MarcaVino
+    Where id = idMarca;
+
+    INSERT INTO MuestraCatador VALUES (
+      pid,
+      idMarca,
+      idClasificacionVino,
+      idJuez,
+      idEdicion,
+      idCatadorExperto,
+      anada,
+      sumatoriaExperto
+    );
+    DBMS_OUTPUT.PUT_LINE('------------------------------------------------');
+    DBMS_OUTPUT.PUT_LINE('Muestra insertada (id = ' || to_char(pid) || ')');
+    DBMS_OUTPUT.PUT_LINE('------------------------------------------------');
+
+
+end;
