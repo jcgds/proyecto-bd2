8a46dbc1e4439c28fc6ad81f2e60aee6fa8f7859
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-17 09:01:47 -0400

    Comentarios de cambios necesarios

--- Procedures/inscribir_bodega.sql
+++ Procedures/inscribir_bodega.sql
@@ -4,6 +4,8 @@ SET VERIFY OFF;
 ACCEPT idBodega PROMPT 'Ingrese el id de la bodega a inscribir: ';
 ACCEPT idEdicion PROMPT 'Ingrese el id de la edicion a la cual inscribir la bodega: ';
 
+-- TODO: Mezclar con datos de muestra compite, para inscribirse debe colocar todos los datos
+--       como en el formulario de inscripcion de las bodegas
 begin
 
     inscribir_bodega(&idBodega, &idEdicion);
--- Procedures/inscribir_catador.sql
+++ Procedures/inscribir_catador.sql
@@ -4,6 +4,9 @@ SET VERIFY OFF;
 ACCEPT idCatador PROMPT 'Ingrese el id del catador a inscribir: ';
 ACCEPT idEdicion PROMPT 'Ingrese el id de la edicion a la cual inscribir al catador: ';
 
+-- TODO: Pedir datos para registrar CatadorAprendiz, esto es como la ficha de inscripcion a un torneo de
+--       catadores
+
 begin
 
     inscribir_catador(&idCatador, &idEdicion);
