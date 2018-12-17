72dba599b71efc27bf61087d4a1b787c6e513062
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-17 09:02:14 -0400

    Agregadas comillas a atributos varchar2 pasados al procedimiento.

--- Procedures/insertar_catador_aprendiz.sql
+++ Procedures/insertar_catador_aprendiz.sql
@@ -5,13 +5,13 @@ ACCEPT pasaporte PROMPT 'Ingrese su numero de pasaporte: ';
 ACCEPT nombre PROMPT 'Ingrese su nombre: ';
 ACCEPT apellido PROMPT 'Ingrese su apellido: ';
 ACCEPT fecha PROMPT 'Ingrese su fecha de nacimiento en el formato dia/mes/año : ';
-ACCEPT genero PROMPT 'Ingrese su genero, M masculino o F femenino: ';
+ACCEPT genero PROMPT 'Ingrese su genero (M/F/O): ';
 ACCEPT ciudad PROMPT 'Ingrese su ciudad de nacimiento: ';
 ACCEPT pais PROMPT 'Ingrese su pais de nacimiento: ';
 
 begin
 
-    insertar_catador_aprendiz(&pasaporte, &nombre, &apellido, &fecha, &genero, &ciudad, &pais);
+    insertar_catador_aprendiz(&pasaporte, '&nombre', '&apellido', '&fecha', '&genero', '&ciudad', '&pais');
 
 end;
 /
