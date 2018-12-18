c038b768f3ef450f2e485326fb5ea94766a469d2
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2018-12-18 10:03:29 -0400

    Input para crear cata aprendiz

---
+++ Procedures/Crear_cata_aprendiz.sql
@@ -0,0 +1,41 @@
+SET SERVEROUTPUT ON;
+SET VERIFY OFF;
+
+
+ACCEPT idCatadorAprendiz PROMPT 'Ingrese el pasaporte del catador aprendiz: ';
+ACCEPT idMuestra PROMPT 'Ingrese el id de la muestra: ';
+ACCEPT idMarca PROMPT 'Ingrese el id de la marca: ';
+ACCEPT idEdicion PROMPT 'Ingrese el id de la edicion: ';
+ACCEPT idCatadorExperto PROMPT 'Ingrese el id del CatadorExperto: ';
+
+declare
+    saveId number := ids_seq.nextval;
+    clasifVino number;
+    idMarcaa number := &idMarca;
+begin
+
+    select fk_clasificacionvinos into clasifVino
+    from MarcaVino where id = idMarcaa;
+
+    insert into CataAprendiz values (
+        saveId,
+        to_date('10-10-2010', 'DD-MM-YYYY'),
+        valoracion_nt(
+            valoracion(null, 0, null)
+        ),
+        null,
+        &idCatadorAprendiz,
+        &idMuestra,
+        idMarcaa,
+        clasifVino,
+        &idEdicion,
+        &idCatadorExperto
+    );
+
+    DBMS_OUTPUT.PUT_LINE('Cata aprendiz registrada id: ' || to_char(saveId));
+    DBMS_OUTPUT.PUT_LINE('Para insertar valoraciones utilizar @Procedures/insertar_valoracion_aprendiz');
+
+end;
+/
+
+SET VERIFY ON;
