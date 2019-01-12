c9aed61f8e31f3d77fd4553a63787ab348182309
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2019-01-12 17:05:00 -0400

    Eliminada tabla I_tiempoAux. Atributo renombrado por que era muy largo para el manejador.

--- DSS/InitI/drop.sql
+++ DSS/InitI/drop.sql
@@ -11,7 +11,6 @@ drop sequence seq_Itipo_concurso;
 drop sequence seq_Imetricas_pais;
 drop sequence seq_Imetricas_concurso;
 
-drop table I_tiempoAux cascade constraints purge;
 drop table I_bodega cascade constraints purge;
 drop table I_marca cascade constraints purge;
 drop table I_critica cascade constraints purge;
