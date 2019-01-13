c569eaf0b34fbfc3a395a6bed4ba306014a2af0e
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2019-01-13 08:38:15 -0400

    Implementada extraccion de criticas

--- DSS/procedimientos.sql
+++ DSS/procedimientos.sql
@@ -4,22 +4,21 @@ anio_max number := 2018;
 idForPaisAux number;
 idForBodega number;
 idForMarca number;
+idForCritica number;
 begin
   
   -- TODO: Calcular estas fechas en base de la tabla de tiempoAux
   << anio_loop >>
   for anio in anio_min .. anio_max loop
-      
-      
+            
       begin
         insert into I_tiempoAux values (anio);
       exception
         when DUP_VAL_ON_INDEX then
-          continue;
+            DBMS_OUTPUT.PUT_LINE('Informacion del ' || to_char(anio) || ' ya extraida.');
+            continue;
       end;
-      
-
-    
+         
     <<paises_loop>>
     for recPais in (select p.id, p.nombre, p.continente, produccion_pais_en(p.id, anio) prod, CalcularExportacionPais(p.id, anio) exportacion from Pais p)
     loop
@@ -44,15 +43,22 @@ begin
 
             <<marca_loop>>
             for recMarca in (                
-                select distinct M.id, M.nombre, produccion_marca_en(M.id, anio) produccion, exportacion_marca_en(M.id, anio) exportacion, premios_marca_en(M.id, anio) nPremios 
+                select distinct M.id, M.nombre, produccion_marca_en(M.id, anio) produccion, exportacion_marca_en(M.id, anio) exportacion, premios_marca_en(M.id, anio) nPremios
                 from MarcaVino M, MarcaVino_B_DO MB,Bodega B 
                 where B.id = MB.fk_bodega and M.id = MB.fk_marcavino and B.id = recBodega.id
             ) loop
-                DBMS_OUTPUT.PUT_LINE('ID: ' || to_char(recMarca.id) || ' Nombre Marca: ' || recMarca.nombre || ' Prod: ' || to_char(recMarca.produccion) || ' Exp: ' || to_char(recMarca.exportacion) || ' Premios: ' || to_char(recMarca.nPremios));
                 idForMarca := seq_Itiempo.nextval; -- TODO: Cambiar nombre de secuencia?
+                DBMS_OUTPUT.PUT_LINE('ID: ' || to_char(recMarca.id) || ' Nombre Marca: ' || recMarca.nombre || ' Prod: ' || to_char(recMarca.produccion) || ' Exp: ' || to_char(recMarca.exportacion) || ' Premios: ' || to_char(recMarca.nPremios));
                 insert into I_marca values (idForMarca, idForBodega, anio, recMarca.nombre, recMarca.produccion, recMarca.exportacion, recMarca.nPremios);
             
-                -- TODO: Extraer criticas
+                << criticas_loop >>
+                for recCritica in (select xx.* from the (select mm.criticas from MarcaVino mm where mm.id = recMarca.id) xx where xx.valor.anio = anio)
+                loop
+                    DBMS_OUTPUT.PUT_LINE('Critica: ' || recCritica.nombreCritica || ' Anio: ' || to_char(recCritica.valor.anio) || ' Valor: ' || to_char(recCritica.valor.valor));
+                    idForCritica := seq_Itiempo.nextval;
+                    INSERT INTO I_critica VALUES (idForCritica, idForMarca, recCritica.valor.valor);
+
+                end loop criticas_loop;
 
             end loop marca_loop;
 
