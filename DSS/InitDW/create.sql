106a72ac5dc63b7159a9ba684f23a72689ad6d80
Juan Carlos Goncalves <juan.goncalves498@gmail.com>
2019-01-13 21:25:15 -0400

    Cambiado nombre de atributo que era muy largo

--- DSS/InitDW/create.sql
+++ DSS/InitDW/create.sql
@@ -2,31 +2,31 @@ CREATE SEQUENCE seq_pais
  START WITH     1
  INCREMENT BY   1
  NOCACHE
- NOCYCLE;
+ NOCYCLE
 /
  CREATE SEQUENCE seq_tiempo
  START WITH     1
  INCREMENT BY   1
  NOCACHE
- NOCYCLE;
+ NOCYCLE
 /
  CREATE SEQUENCE seq_tipo_concurso
  START WITH     1
  INCREMENT BY   1
  NOCACHE
- NOCYCLE;
+ NOCYCLE
 /
  CREATE SEQUENCE seq_metricas_pais
  START WITH     1
  INCREMENT BY   1
  NOCACHE
- NOCYCLE;
+ NOCYCLE
 /
  CREATE SEQUENCE seq_metricas_concurso
  START WITH     1
  INCREMENT BY   1
  NOCACHE
- NOCYCLE;
+ NOCYCLE
 /
 create table DW_metricas_pais (
     id number (10),
@@ -50,14 +50,14 @@ create table DW_metricas_pais (
     top2_marcas_criticas varchar2(50),
     top3_marcas_criticas varchar2(50),
     constraint DW_metricas_pais primary key (id)
-);
+)
 /
 create table DW_metricas_concurso (
     id number (10),
     id_tipo_concurso number(10),
     id_tiempo number(10),
     id_lugar number(10),
-    PorcentajeCrecimiento_TipoConcurso number(3),
+    PorcCrecimiento_TipoConcurso number(3),
     top1_marca_premios varchar(50),
     top2_marca_premios varchar(50),
     top3_marca_premios varchar(50),
@@ -66,14 +66,14 @@ create table DW_metricas_concurso (
     top3_catador varchar(50),
     top_concursoCatadores varchar(50),
     constraint DW_metricas_concurso primary key (id)
-);
+)
 /
 create table DW_tiempo (
     id number (10),
     anio date,
     bienio number (1) not null,
     constraint DW_tiempo primary key (id)
-);
+)
 /
 create table DW_pais (
     id number (10),
@@ -82,12 +82,12 @@ create table DW_pais (
     continente varchar(50) not null,
     fecha_creacion date not null,
     constraint DW_pais primary key (id)
-);
+)
 /
 create table DW_tipo_concurso (
     id number (10),
     nombre varchar(50) not null,
     fecha_creacion date not null,
     constraint DW_tipoConcurso primary key (id)
-);
+)
 /
